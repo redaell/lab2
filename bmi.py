431e5a530497e1fbda489977a7762f9977a7256b
dejun <hengdj.22@ichat.sp.edu.sg>
2024-10-30 10:43:09 +0800

    Added bmi.py

---
+++ bmi.py
@@ -0,0 +1,14 @@
+def calculate_bmi(height,weight):
+    print("Height =" + str(height))
+    print("Weight =" + str(weight))
+    BMI = weight/(height **2)
+    print ("BMI = ", round(BMI))
+    print ("Weight Classification: ", end="")
+    if BMI < 18.5:
+        print("Under Weight")
+    elif BMI >25.0:
+        print("Over Weight")
+    else:
+        print("Normal Weight")
+
+calculate_bmi(weight=57, height=1.73)
